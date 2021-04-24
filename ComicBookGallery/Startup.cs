8be87cde12a6d805f5ff84657daf19b7bc7e766c
Skojos <62700429+Skojos@users.noreply.github.com>
2021-04-24 13:31:59 +0200

    Added design

--- ComicBookGallery/Startup.cs
+++ ComicBookGallery/Startup.cs
@@ -50,7 +50,7 @@ namespace ComicBookGallery
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=ComicBooks}/{action=Index}/{id?}");
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
             });
         }
     }
