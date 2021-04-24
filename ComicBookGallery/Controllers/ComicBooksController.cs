915d50e1591b477b50ec95df1167a3beb7953b93
Skojos <62700429+Skojos@users.noreply.github.com>
2021-04-24 21:55:32 +0200

    updated comic book detail view to be strongly typed

--- ComicBookGallery/Controllers/ComicBooksController.cs
+++ ComicBookGallery/Controllers/ComicBooksController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ComicBookGallery.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace ComicBookGallery.Controllers
@@ -19,19 +20,24 @@ namespace ComicBookGallery.Controllers
         public ActionResult Detail()
         {
 
-            var seriesTitle = "The Amazing Spider-Man";
-            var issueNumber = 700;
-            var description = "<p>Final issue! Witness the final hours of Doctor Octopus' life and his one, last, great act of revenge! Even if Spider-Man survives... <strong>will Peter Parker?</strong></p>";
-            var artists = new string[]
+            var comicBook = new ComicBook()
             {
-        "Script: Dan Slott",
-        "Pencils: Humberto Ramos",
-        "Inks: Victor Olazaba",
-        "Colors: Edgar Delgado",
-        "Letters: Chris Eliopoulos"
+                SeriesTitle = "The Amazing Spider-Man",
+                IssueNumber = 700,
+                DescriptionHtml = "<p>Final issue! Witness the final hours of Doctor Octopus' life and his one, last, great act of revenge! Even if Spider-Man survives... <strong>will Peter Parker?</strong></p>",
+                Artist = new Artist[]
+                { 
+                    new Artist() { Name = "Dan Slott", Role = "Script"},
+                    new Artist() { Name = "Humberto Ramos", Role = "Pencils"},
+                    new Artist() { Name = "Victor Olazaba", Role = "Inks"},
+                    new Artist() { Name = "Edgar Delgado", Role = "Colors"},
+                    new Artist() { Name = "Chris Eliopoulos", Role = "Letters"},
+                }
+
             };
-        
-            return View();
+
+       
+            return View(comicBook);
         }
 
     }
